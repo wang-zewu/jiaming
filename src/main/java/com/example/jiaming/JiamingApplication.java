8550d8cf44aa05d8c8527988b90d38b96ff8077a
wangjiaming <wangjiaming@ane56.com>
2019-03-02 16:51:39 +0800

    自有项目提交到github

---
+++ src/main/java/com/example/jiaming/JiamingApplication.java
@@ -0,0 +1,13 @@
+package com.example.jiaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiamingApplication.class, args);
+	}
+
+}
