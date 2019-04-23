1f01aeec7582a3a72b55292e36219af18bb91e16
wangjiaming <wangjiaming@ane56.com>
2019-04-23 16:14:21 +0800

    自有项目提交到github

--- src/test/java/com/example/jiaming/JiamingApplicationTests.java
+++ src/test/java/com/example/jiaming/JiamingApplicationTests.java
@@ -2,15 +2,18 @@ package com.example.jiaming;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@SpringBootConfiguration
 public class JiamingApplicationTests {
 
     @Test
     public void contextLoads() {
+        System.out.println("dfdsfsdfds");
     }
 
 }
